77a419df0bb112f3d76d1fc15b58e2d29d455389
Martin Chaijale <chaijalemartin@gmail.com>
2022-11-05 18:01:38 -0300

    nuevo proyecto agregando js para iniciar sesion

--- js/app.js
+++ js/app.js
@@ -9,6 +9,7 @@ const verificar = (edad) => {
     alert("Necesitaras que un mayor valide tu compra");
   }
 };
+
 //Funcion para que el usuario selectocciones que tipo tipo de producto necesita
 const seleccionProducto = (tipoProducto) => {
   while (
@@ -51,18 +52,102 @@ const seleccionProducto = (tipoProducto) => {
     }
   }
 };
+//funcion constructora de usuarios
+function Usuario(nombre, constrasena, email, edad) {
+  this.nombre = nombre;
+  this.contrasena = constrasena;
+  this.email = email;
+  this.edad = edad;
+}
 
-let nombreUsuario = prompt("Ingrese su nombre").toUpperCase();
-saludar(nombreUsuario);
-let edadUsuario = Number(prompt("Ingresa tu edad"));
-verificar(edadUsuario);
-let producto;
-seleccionProducto(producto);
-let respuesta = prompt("¿Desea realizar otra consulta?: ").toLowerCase();
-while (respuesta == "si") {
-  if (respuesta == "si") {
-    seleccionProducto(producto);
-    respuesta = prompt("¿Desea realizar otra consulta? (ingrese si para continuar): ").toLowerCase();
+const usuarios = [
+  {
+    nombre: 'martin',
+    contrasena: '123',
+    email: 'martin@yourcomputer.com',
+    edad: 29,
+  },
+  {
+    nombre: 'gonzalo',
+    contrasena: '145',
+    email: 'gonzalo@yourcomputer.com',
+    edad: 32,
+  },
+  {
+    nombre: 'florencia',
+    contrasena: '456',
+    email: 'florencia@yourcomputer.com',
+    edad: 24,
+  },
+];
+
+let user;
+do {
+  user = prompt("Bienvenido a YOURCOMPUTER. ¿Sos nuevo?").toLowerCase();
+} while (user != "si" && user != "no");
+if (user == 'si') {
+  let nuevoUsuario = prompt("Porfavor ingresa tu nombre de usuario").toLowerCase();
+  let verificador = usuarios.some((nombre) => nombre.nombre === nuevoUsuario);
+
+  while (verificador == true) {
+    alert("Usuario ya existente. Prueba con otro nombre");
+    nuevoUsuario = prompt("Porfavor ingresa tu nombre de usuario").toLowerCase();
+    verificador = usuarios.some((nombre) => nombre === nuevoUsuario);
+  }
+
+  let nuevaClave = prompt("Porfavor ingresa tu contrasena").toLowerCase();
+
+  let nuevoEmail = prompt("Porfavor ingresa tu email").toLowerCase();
+  verificador = false;
+  verificador = usuarios.some((email) => email.email === nuevoEmail);
+  while (verificador == true) {
+    alert("Email ya existente. Prueba con otro correo");
+    nuevoEmail = prompt("Porfavor ingresa tu email").toLowerCase();
+    verificador = usuarios.some((email) => email.email === nuevoEmail);
+  }
+
+  let nuevaEdad = Number(prompt("Porfavor ingresa tu edad"));
+  verificar(nuevaEdad);
+  alert("Perfecto! ya fuiste cargado a nuestro sistema. A constinuacion ingresa con tus datos.");
+
+  usuarios.push(new Usuario(nuevoUsuario, nuevaClave, nuevoEmail, nuevaEdad));
+  console.log(usuarios)
+}
+user = prompt("Nombre de usuario: ");
+let buscarUser = usuarios.some((usuario) => usuario.nombre === user)
+let indice = usuarios.indexOf(usuarios.find((usuario) => usuario.nombre == user))
+
+while (buscarUser != true) {
+  alert("Usuario inexistente, porfavor vuelva a intentarlo");
+  user = prompt("Nombre de usuario: ");
+  buscarUser = usuarios.some((usuario) => usuario.nombre === user)
+  indice = usuarios.indexOf(usuarios.find((usuario) => usuario.nombre == user))
+}
+let pass = prompt("Ingresa tu contraseña: ");
+let intentos = 0
+while (pass != usuarios[indice].contrasena && intentos <= 3) {
+  alert("Contraseña incorrecta, vuelve a intentarlo");
+  pass = prompt("Ingresa tu contraseña: ");
+  intentos++
+  if (intentos == 3) {
+    alert("Ultimo intento");
+
   }
 }
-alert("Muchas gracias por tu visita " + nombreUsuario + ". Nos vemos pronto.");
+if (intentos <= 3) {
+  saludar(usuarios[indice].nombre);
+  let producto;
+  seleccionProducto(producto);
+  let respuesta = prompt("¿Desea realizar otra consulta?: ").toLowerCase();
+  while (respuesta == "si") {
+    if (respuesta == "si") {
+      seleccionProducto(producto);
+      respuesta = prompt(
+        "¿Desea realizar otra consulta? (ingrese si para continuar): "
+      ).toLowerCase();
+    }
+  }
+  alert("Muchas gracias por tu visita " + usuarios[indice].nombre + ". Nos vemos pronto.");
+} else {
+  alert("No pudimos iniciar sesion. Ponte en contacto con soporte.Muchas gracias");
+}
